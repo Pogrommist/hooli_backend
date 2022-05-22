4989b399124fb2732daae548c961a5a5dbd38930
Alexey Arbuzov <91293alex@gmail.com>
2022-05-22 23:27:47 +0300

    Add devise-jwt gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+  before_action :set_default_response_format
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  respond_to :json
+
+  private
+
+  def set_default_response_format
+    request.format = :json
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
