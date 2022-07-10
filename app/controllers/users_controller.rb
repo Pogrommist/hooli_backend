18588824b627da30c1f8823c896fee3bfaaa2576
Maxim Naumkin <naumkin.maks@mail.ru>
2022-07-10 18:17:33 +0300

    add update_user_info method to users_controller.rb to be able to update user's info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,28 @@
 class UsersController < ApplicationController 
-   before_action :authenticate_user!, only: [:show, :upload_avatar]
-    def show 
-        render json: current_user
-    end
+	before_action :authenticate_user!, only: [:show, :upload_avatar, :update_user_info]
 
-    def upload_avatar
-        avatar = Cloudinary::Uploader.upload(params[:avatar_url], :folder => "users_avatars")
+	def show 
+			render json: current_user
+	end
 
-        current_user.update(avatar_url: avatar["secure_url"])
+	def upload_avatar
+		avatar = Cloudinary::Uploader.upload(params[:avatar_url], :folder => "users_avatars")
 
-        render json: current_user
-    end
+		current_user.update(avatar_url: avatar["secure_url"])
+
+		render json: current_user
+	end
+
+	def update_user_info
+		current_user.update(params_user)
+
+		render json:  current_user
+	end
+
+	private
+
+	def params_user
+		params.require(:user).permit(:first_name, :last_name)
+	end
+
 end
