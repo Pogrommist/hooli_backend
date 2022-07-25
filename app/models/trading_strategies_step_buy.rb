0d219bf5ed6920a7567556dbceeac752ffae3963
Maxim Naumkin <naumkin.maks@mail.ru>
2022-07-25 15:08:07 +0300

    Cheated consecutive integers when defining an enum

--- app/models/trading_strategies_step_buy.rb
+++ app/models/trading_strategies_step_buy.rb
@@ -1,5 +1,5 @@
 class TradingStrategiesStepBuy < ApplicationRecord
   has_many :base_trading_strategies_step_items, as: :strategy_item
 
-  enum status: { completed: 0, not_completed: 1, running: 2 }
+  enum status: { completed: 10, not_completed: 20, running: 30 }
 end
