9482b1a3f04e915976dd0b3cd2488fca6226dd55
Maxim Naumkin <naumkin.maks@mail.ru>
2022-07-31 19:13:41 +0300

    Changed the show, update, destroy methods in the BaseTradingStrategiesStepItemsController controller

--- app/controllers/base_trading_strategies_step_items_controller.rb
+++ app/controllers/base_trading_strategies_step_items_controller.rb
@@ -12,31 +12,33 @@ class BaseTradingStrategiesStepItemsController < ApplicationController
   end
   
   def show
-    # ToDo: use 'create' method as example and fix next line
-    @base_trading_strategies_step_item = current_user.trading_strategies.base_trading_strategies_step_items.find_by(id: params[:id])
+    strategy = current_user.trading_strategies.find_by(id: params[:strategy_id])
+    step_service = StrategyService::StrategyStepsService::StrategyStepGetter.new(strategy, params)
+    step_model = step_service.get_step_object
+    step_params = step_service.permit_step_params
+    step = step_model
 
-    render json: @base_trading_strategies_step_item
+    render json: step || {}
   end
 
   def update
-    # ToDo: use 'create' method as example and fix next line
-    @base_trading_strategies_step_item = current_user.trading_strategy.base_trading_strategies_step_items.find_by(id: params[:id])&.update(base_trading_strategies_step_item_params)
+    strategy = current_user.trading_strategies.find_by(id: params[:strategy_id])
+    step_service = StrategyService::StrategyStepsService::StrategyStepGetter.new(strategy, params)
+    step_model = step_service.get_step_object
+    step_params = step_service.permit_step_params
+    step = step_model&.update(step_params)
 
-    render json: @base_trading_strategies_step_item
+    render json: step || {}
   end
 
   def destroy
-    # ToDo: use 'create' method as example and fix next line
-    @base_trading_strategies_step_item = current_user.trading_strategy.base_trading_strategies_step_items.find_by(id: params[:id])&.destroy
+    strategy = current_user.trading_strategies.find_by(id: params[:strategy_id])
+    step_service = StrategyService::StrategyStepsService::StrategyStepGetter.new(strategy, params)
+    step_model = step_service.get_step_object
+    step_params = step_service.permit_step_params
+    step = step_model&.destroy(step_params)
 
-    render json: @base_trading_strategies_step_item
-  end
-
-  private 
-  
-  def base_trading_strategies_step_item_params
-    # ToDo: remove that method and use StrategyStepGetter instead
-    params.require(:strategy_step).permit(:strategy_item, :trading_strategy)
+    render json: step || {}
   end
 end
 
